2ac1d51354d7c257264955bdb0d05c3efd55f4ea
Alec Reid <alecreid0093@gmail.com>
2019-09-15 02:41:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,21 +5,21 @@
 
 
 def introduction(name)
-  puts "Hi, my name is #{name}"
+  puts "Hi, my name is #{name}."
 end
 
 introduction('Josh')
 introduction('Dan')
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name}, and I am learning to program in #{language}"
+  puts "Hi, my name is #{name}, and I am learning to program in #{language}."
 end
 
 introduction_with_language("Dan", "Ember.js")
 introduction_with_language("Josh", "React")
 
 def introduction_with_language_optional(name, language = "Ruby")
-  puts "Hi, my name is #{name} "
+  puts "Hi, my name is #{name} and I am learning to code in #{language}."
 
 
 
